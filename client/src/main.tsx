ca4b39c3ac188c279ad164a617e57d732980e744
Arthur Karambizi <arthurkarambizi2@gmail.com>
2024-02-02 23:46:50 -0800

    Update Vite configuration and main.tsx file

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { ThemeProvider } from './ThemeProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
+);

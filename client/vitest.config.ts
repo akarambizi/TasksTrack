061861d5e48fa6f5689fee118b2f8dcc04b7b237
Arthur Karambizi <arthurkarambizi2@gmail.com>
2024-04-28 19:02:19 -0700

    Refactor .gitignore files and added vitest

---
+++ client/vitest.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            globals: true, // describe, it, expect, beforeEach, afterEach
+            environment: 'happy-dom', // fix document is not defined
+            coverage: {
+                provider: 'istanbul',
+                include: ['src/components/**/*.{js,jsx,ts,tsx}'],
+                exclude: [
+                    "src/components/**/*.{types,stories,constants,test,spec}.{js,jsx,ts,tsx}",
+                    "src/components/ui"
+                ],
+                thresholds: {
+                    global: {
+                        branches: 80,
+                        functions: 80,
+                        lines: 80,
+                        statements: 80
+                    }
+                }
+            }
+        }
+    })
+);

0d5dabbc85e5ffb882df6da87372214b86bc7d1e
Arthur Karambizi <arthurkarambizi2@gmail.com>
2024-07-25 23:18:39 -0700

    Refactor userAuth hooks and update API imports

--- client/src/hooks/userAuth.ts
+++ client/src/hooks/userAuth.ts
@@ -1,8 +1,32 @@
-import { registerUser } from "@/api";
-import { useMutation } from "react-query";
+import { IUserData, loginUser, logoutUser, registerUser, requestPasswordReset, resetPasswordWithToken } from '@/api';
+import { useMutation } from 'react-query';
 
 export const useRegisterUser = () => {
     return useMutation({
-        mutationFn: (userData: { email: string; password: string }) => registerUser(userData)
+        mutationFn: (userData: IUserData) => registerUser(userData)
+    });
+};
+
+export const useLoginUser = () => {
+    return useMutation({
+        mutationFn: (userData: IUserData) => loginUser(userData)
+    });
+};
+
+export const useLogoutUser = () => {
+    return useMutation({
+        mutationFn: () => logoutUser()
+    });
+};
+
+export const useRequestPasswordReset = () => {
+    return useMutation({
+        mutationFn: (email: string) => requestPasswordReset(email)
+    });
+};
+
+export const useResetPasswordWithToken = () => {
+    return useMutation({
+        mutationFn: ({ newPassword, token }: { newPassword: string; token: string }) => resetPasswordWithToken(newPassword, token)
     });
 };
